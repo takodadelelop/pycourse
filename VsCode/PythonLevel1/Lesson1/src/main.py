297f6adcaad060974ed20bc84db48f439b4f4291
Damiano <takodadevelop@gmail.com>
2025-03-15 19:26:26 +0100

    Primo

---
+++ VsCode/PythonLevel1/Lesson1/src/main.py
@@ -0,0 +1,11 @@
+titolo = "Isola misteriosa"
+autore = "Giulio Verne"
+print(f"Titolo: {titolo.upper()}, Autore:{autore}.")
+#PEP 498 interpolazione di stringhe
+#Operatori
+#Aritmentici + -* (/ int) (/ float) (mod %) (exponent **) (men unario -)
+# Assegnamento = += -= *= /= //= %= **=
+# Confronto < > == != >= <=
+#Logici and or not 
+
+
